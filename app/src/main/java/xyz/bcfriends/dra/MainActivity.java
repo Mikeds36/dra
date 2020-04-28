80154394920c8eae1a21acd4388430a20f2bab0a
dora-0 <55905774+dora-0@users.noreply.github.com>
2020-04-28 18:36:03 +0900

    Push 알림 구현

--- app/src/main/java/xyz/bcfriends/dra/MainActivity.java
+++ app/src/main/java/xyz/bcfriends/dra/MainActivity.java
@@ -1,13 +1,54 @@
 package xyz.bcfriends.dra;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.NotificationCompat;
 
 public class MainActivity extends AppCompatActivity {
+    final String CHANNEL_ID = "TEST";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final NotificationManager notificationManager = (NotificationManager) MainActivity.this.getSystemService(NOTIFICATION_SERVICE);
+        final Intent intent = new Intent(MainActivity.this.getApplicationContext(), MainActivity.class);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CharSequence name = getString(R.string.channel_name);
+        String description = getString(R.string.channel_description);
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+        channel.setDescription(description);
+        // Register the channel with the system; you can't change the importance
+        // or other notification behaviors after this
+        notificationManager.createNotificationChannel(channel);
+
+        Button btn = findViewById(R.id.push_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.notification_icon)
+                        .setContentTitle("알림 테스트")
+                        .setContentText("Hello World!")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        // Set the intent that will fire when the user taps the notification
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true);
+
+                notificationManager.notify(1, builder.build());
+            }
+        });
     }
 }
