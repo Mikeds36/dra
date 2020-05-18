f0db9e269c4f7afc7402654da07aad30ecfdaa05
Mikeds <darponge@gmail.com>
2020-05-18 20:49:03 +0900

    Notification 파일 이동, prolificinteractive/material-calendarview 추가

--- app/src/main/java/xyz/bcfriends/dra/MainActivity.java
+++ app/src/main/java/xyz/bcfriends/dra/MainActivity.java
@@ -1,59 +1,15 @@
 package xyz.bcfriends.dra;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import androidx.core.app.NotificationCompat;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    final String CHANNEL_ID = "TEST";
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        final NotificationManager notificationManager = (NotificationManager) MainActivity.this.getSystemService(NOTIFICATION_SERVICE);
-        final Intent intent = new Intent(this, MainActivity.class);
-
-//        Intent testIntent = new Intent(this, MainActivity.class);
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        CharSequence name = getString(R.string.channel_name);
-        String description = getString(R.string.channel_description);
-        int importance = NotificationManager.IMPORTANCE_DEFAULT;
-        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-        channel.setDescription(description);
-        // Register the channel with the system; you can't change the importance
-        // or other notification behaviors after this
-        notificationManager.createNotificationChannel(channel);
-
-        Button btn = findViewById(R.id.push_btn);
-        /* TODO:
-            - 알림을 클릭하면 팝업화면이 나와서 버튼을 클릭할 수 있게 하기
-        */
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
-
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
-                        .setSmallIcon(R.drawable.notification_icon)
-                        .setContentTitle("Dra")
-                        .setContentText("오늘의 기분은 어떠신가요?")
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                        // Set the intent that will fire when the user taps the notification
-                        .setContentIntent(pendingIntent)
-                        .setAutoCancel(true);
-
-                notificationManager.notify(1, builder.build());
-            }
-        });
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.calendar);
     }
 }
