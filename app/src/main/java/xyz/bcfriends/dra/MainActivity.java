91108c5add64a3bf54afb5373bd56c4737d09a28
dora-0 <55905774+dora-0@users.noreply.github.com>
2020-05-18 22:56:48 +0900

    fix calendar

--- app/src/main/java/xyz/bcfriends/dra/MainActivity.java
+++ app/src/main/java/xyz/bcfriends/dra/MainActivity.java
@@ -1,17 +1,12 @@
 package xyz.bcfriends.dra;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.widget.CalendarView;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar);
-
     }
 }
