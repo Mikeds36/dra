7bbad2ca1bd614299b15ca90a95c001d86fac846
Mikeds <darponge@gmail.com>
2020-05-24 16:17:05 +0900

    Added Custom AppBar  - Added Appbar layout Move MainActivity to NotiFragment Added Hamburger Menu with some picture Clean up activity_main.xml  - split layouts Clean up Values  - String  - Dimens

--- app/src/main/java/xyz/bcfriends/dra/MainActivity.java
+++ app/src/main/java/xyz/bcfriends/dra/MainActivity.java
@@ -1,43 +1,53 @@
 package xyz.bcfriends.dra;
 
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.NotificationCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    final String CHANNEL_ID = "main";
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final NotificationImpl noti = new NotificationImpl(this);
-        noti.createNotificationChannel(CHANNEL_ID, "알림 채널", "테스트용 알림 채널");
-        Button btn = findViewById(R.id.push_btn);
-        final Intent intent = new Intent(this, MainActivity.class);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
 
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
-                        .setSmallIcon(R.drawable.notification_icon)
-                        .setContentTitle("Dra")
-                        .setContentText("오늘의 기분은 어떠신가요?")
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                        // Set the intent that will fire when the user taps the notification
-                        .setContentIntent(pendingIntent)
-                        .setAutoCancel(true);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_stat, R.id.nav_share)
+                .setDrawerLayout(drawer)
+                .build();
 
-                noti.notify("test", builder.build());
-            }
-        });
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@Nullable View parent, @NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
+        return super.onCreateView(parent, name, context, attrs);
     }
 }
---
+++ app/src/main/java/xyz/bcfriends/dra/NotiFragment.java
@@ -0,0 +1,64 @@
+package xyz.bcfriends.dra;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.core.app.NotificationCompat;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class NotiFragment extends Fragment {
+    private static final String CHANNEL_ID = "main";
+
+    public NotiFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        Activity mainActivity = requireActivity();
+        View v = inflater.inflate(R.layout.notification_fragment, container, false);
+
+        final NotificationImpl noti = new NotificationImpl(mainActivity);
+        noti.createNotificationChannel(CHANNEL_ID, "알림 채널", "테스트용 알림 채널");
+
+        Button btn = v.findViewById(R.id.push_btn);
+        final Intent intent = new Intent(mainActivity, MainActivity.class);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Activity mainActivity = requireActivity();
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                PendingIntent pendingIntent = PendingIntent.getActivity(mainActivity, 0, intent, 0);
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(mainActivity, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.notification_icon)
+                        .setContentTitle("Dra")
+                        .setContentText("오늘의 기분은 어떠신가요?")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        // Set the intent that will fire when the user taps the notification
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true);
+
+                noti.notify("test", builder.build());
+            }
+        });
+
+        return inflater.inflate(R.layout.activity_main, container, false);
+    }
+}
